be45575ee41bc032c35723f71c4d939ece60a077
bryan.abate@appdynamics.com <bryan.abate@appdynamics.com>
2020-12-14 12:29:28 +0100

    Moving starship

--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,23 @@
 import { Graphics } from './graphics';
 import { Controls } from "./controls";
 import { EventBus } from "./events";
+import { GameScene } from 'gameScene';
 
 export class GameLoop {
 
     private stopped = true;
     graphics: Graphics;
+    private lastTime: number;
         
     constructor(
             private readonly eventBus: EventBus,
-            private readonly controls: Controls) {
+            private readonly controls: Controls,
+            private readonly scene: GameScene) {
 
         // TODO: for debug only, remove and handle fire somewhere else
         this.eventBus.on('fire', () => console.log('pew!'));
         this.graphics = new Graphics();
+        this.lastTime = (new Date()).getTime();
     }
 
     stop() {
@@ -29,6 +33,12 @@ export class GameLoop {
         if (this.stopped) {
             return;
         }
+
+        const newTime = (new Date()).getTime();
+
+        this.scene.tick(newTime - this.lastTime);
+        this.lastTime = newTime;
+
         requestAnimationFrame(this.loop);
 
         console.log('throttle: ' + this.controls.getCurrentControls().engineThrottle); 
